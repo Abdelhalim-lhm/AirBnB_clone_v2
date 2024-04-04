7be70b5b0e7841e1ab02ddcda9ceb583c99135dd
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2024-04-04 14:48:04 -0700

    puppet

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,74 @@
+# setting the web_static using puppet
+
+package { 'nginx':
+  ensure => present,
+}
+
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+}
+
+exec { 'create_data':
+  command  => 'sudo mkdir -p /data/',
+  provider => shell,
+}
+
+exec { 'create_web_static':
+  command  => 'sudo mkdir -p /data/web_static/',
+  provider => shell,
+}
+
+exec { 'create_release':
+  command  => 'sudo mkdir -p /data/web_static/releases/',
+  provider => shell,
+}
+
+exec { 'create_shared':
+  command  => 'sudo mkdir -p /data/web_static/shared',
+  provider => shell,
+}
+
+exec { 'create_test':
+  command  => 'sudo mkdir -p /data/web_static/releases/test/',
+  provider => shell,
+}
+
+exec { 'create_index':
+  command  => 'sudo touch /data/web_static/releases/test/index.html',
+  provider => shell,
+}
+
+exec { 'create_index_content':
+  command  => 'sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html',
+  provider => shell,
+}
+
+exec { 'create_link':
+  command  => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
+  provider => shell,
+}
+
+exec { 'give_ownership':
+  command  => 'sudo chown -R ubuntu:ubuntu /data/',
+  provider => shell,
+}
+
+exec { 'default_index':
+  command  => 'sudo sed -i "/listen 80 default_server/a\\
+    \tlocation /hbnb_static / {\
+    \n\t\talias /data/web_static/current/;\
+    \n\t}" /etc/nginx/sites-enabled/default',
+  provider => shell,
+}
+
+exec { 'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
