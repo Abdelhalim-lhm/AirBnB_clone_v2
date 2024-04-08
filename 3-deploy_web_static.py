55e12a4dad093097ad6ec3288cab87ad3e70e812
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2024-04-07 17:23:19 -0700

    3. Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -40,7 +40,7 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(data_path, f_name))
         return True
-    except Exception as e:
+    except:
         return False
 
 
