ce427eb023243b24addbe2b1d5548829c62eb73e
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2024-04-21 12:43:02 -0700

    2. C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" importing FLask """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ Display Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def dusplay_hbnb():
+    """ Display HBNB """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cisfun(text):
+    """ Display c + text """
+    return "C " + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    """ definition of the listening point and port """
+    app.run(host="0.0.0.0", port=5000)
