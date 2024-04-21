3c83cecea493d7a2f2f04b20ecd8b8682503bb47
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2024-04-21 13:21:21 -0700

    5. Number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" importing FLask """
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ Display Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def dusplay_hbnb():
+    """ Display HBNB """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cisfun(text):
+    """ Display c + text """
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """ Display python + text """
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def isnumber(n):
+    """ Display n is a number if n is an integer """
+    if isinstance(n, int):
+        return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    """ definition of the listening point and port """
+    app.run(host="0.0.0.0", port=5000)
